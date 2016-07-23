6c7a6d0b360b1becf135362ce0e8deb2f494e677
Moni Francesca <mo.francesca@gmail.com>
2016-07-24 00:47:12 +0200

    Working on this level still

--- Player.js
+++ Player.js
@@ -1,7 +1,12 @@
 class Player {
+  constructor() {
+    // This code will be executed only once, at the beginning of the level
+    this._health = 20;
+  }
+
   playTurn(warrior) {
-    if (warrior.feel().isEmpty()) {
-      warrior.walk();
+    if (warrior.feel().isEmpty() && warrior.health() < 20) {
+      warrior.rest();
     } else {
       warrior.attack();
     }
