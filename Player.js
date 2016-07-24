fad2103fcde27f143c3269fc134ecac32904dc2b
Moni Francesca <mo.francesca@gmail.com>
2016-07-24 23:50:03 +0200

    cleared level. yay.

--- Player.js
+++ Player.js
@@ -1,12 +1,17 @@
 class Player {
-  constructor() {
-    // This code will be executed only once, at the beginning of the level
-    this._health = 20;
-  }
+  // constructor() {
+  //   // This code will be executed only once, at the beginning of the level
+  //   this._health = 20;
+  // }
 
   playTurn(warrior) {
+    //if health is less than 20 rest
     if (warrior.feel().isEmpty() && warrior.health() < 20) {
       warrior.rest();
+
+      //when health is at 20 walk
+    } else if (warrior.feel().isEmpty() && warrior.health() == 20) {
+      warrior.walk();
     } else {
       warrior.attack();
     }
